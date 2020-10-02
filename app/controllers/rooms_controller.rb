26a83ea756b8e0386a22e4de0b606845e33240e6
manap1031 <ikme.sabosan00@gmail.com>
2020-10-02 13:13:25 +0900

    コントローラー、モデル、ビューファイルの作成

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,27 @@
+class RoomsController < ApplicationController
+
+  def index
+    @rooms = Room.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+  
+end
